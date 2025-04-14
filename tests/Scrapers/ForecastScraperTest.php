23e00eddde7a33d3eeb843e11a30e10ad4805a8a
shimomo <developer@shimomo.net>
2025-04-15 03:24:24 +0900

    test: Rename `testInvalidWithRaceCode1AndDate20250102()` to `testThrowsExceptionWhenMethodDoesNotExist()` in `ForecastScraperTest`

--- tests/Scrapers/ForecastScraperTest.php
+++ tests/Scrapers/ForecastScraperTest.php
@@ -42,7 +42,7 @@ final class ForecastScraperTest extends TestCase
     /**
      * @return void
      */
-    public function testInvalidWithRaceCode1AndDate20250102(): void
+    public function testThrowsExceptionWhenMethodDoesNotExist(): void
     {
         $this->expectException(\BadMethodCallException::class);
         $this->expectExceptionMessage(
